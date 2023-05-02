0f8b1feb1d7aa373934ef0bcfce1d2d88d04e211
zahra <zAHRAkrd7@gamil.com>
2023-05-02 08:55:55 -0700

    initial commit

---
+++ RentalProject/src/Movie.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String genre;
+    private String dirctor;
+    private Date releaseDate;
+    private boolean isAvailble;
+
+
+    public Movie(int id, String title, String genre, String dirctor, Date releaseDate) {
+        this.dirctor = dirctor;
+        this.id = id;
+        this.genre = genre;
+        this.title = title;
+        this.releaseDate = releaseDate;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDirctor() {
+        return dirctor;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public boolean isAvailble() {
+        return isAvailble;
+    }
+
+    public void setAvailble(boolean availble) {
+        isAvailble = availble;
+    }
+}
